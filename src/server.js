699b6f648b09b36ef0f7a9d0ce18d3b65249c678
Oscar Maia <oscarfgmaia@gmail.com>
2022-12-10 18:48:14 +0000

    fix: cors implemented

--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ import customersRouter from "./routes/customersRouter.js"
 import categoriesRouter from "./routes/categoriesRouter.js"
 import rentalsRouter from "./routes/rentalsRouter.js"
 import gamesRouter from "./routes/gamesRouter.js"
-
+import cors from 'cors'
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(customersRouter);
 app.use(categoriesRouter);
