069b26ef64cff06fb5cd1789cb154f057a402564
Oscar Maia <oscarfgmaia@gmail.com>
2022-12-10 21:33:51 +0000

    fix: greater than 0 on gameModel

--- src/models/game.model.js
+++ src/models/game.model.js
@@ -3,8 +3,8 @@ import joi from "joi";
 const gameModel = joi.object({
     name: joi.string().min(3).max(20).required(),
     image: joi.string().uri().required(),
-    categoryId:joi.number().integer().required(),
-    stockTotal: joi.number().integer().required(),
+    categoryId:joi.number().integer().greater(0).required(),
+    stockTotal: joi.number().integer().greater(0).required(),
     pricePerDay: joi.number().required()
 })
 
