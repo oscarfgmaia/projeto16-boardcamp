415ba276aed1976cfda49ef5e231776a0a23962f
Oscar Maia <oscarfgmaia@gmail.com>
2022-12-12 15:43:31 +0000

    feat: rules implementeds

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -2,14 +2,30 @@ import { connectionDb } from "../database/db.js"
 
 export async function create(req, res) {
     const { customerId, gameId, daysRented } = req.body;
+    if(daysRented<=0){
+        return res.sendStatus(400);
+    }
     try {
         //todo change fixed price
+        const gameToBeRented = await connectionDb.query(`
+            SELECT * FROM games WHERE id=$1;
+        `, [gameId])
+        if (gameToBeRented.rowCount === 0) {
+            return res.sendStatus(400);
+        }
+        const customerRenting = await connectionDb.query(`
+        SELECT * FROM customers WHERE id=$1;
+    `, [customerId])
+        if (customerRenting.rowCount === 0) {
+            return res.sendStatus(400);
+        }
+        const priceOfGameToBeRented = gameToBeRented.rows[0].pricePerDay;
         await connectionDb.query(`
         INSERT INTO rentals
             ("customerId","gameId","daysRented","rentDate","returnDate","delayFee","originalPrice")
         VALUES
-            ($1,$2,$3,now()::date,null,null,5);
-    `, [customerId, gameId, daysRented])
+            ($1,$2,$3,now()::date,null,null,$4);
+    `, [customerId, gameId, daysRented, priceOfGameToBeRented * daysRented])
         res.sendStatus(201);
     } catch (error) {
         res.status(500).send(error.message)
@@ -17,6 +33,7 @@ export async function create(req, res) {
 }
 
 export async function findAll(req, res) {
+    const { customerId, gameId } = req.query;
     try {
         const rentals = await connectionDb.query(`
         SELECT *,c.name as customer_name,g.name as g_name,categories.name as category_name
@@ -26,8 +43,8 @@ export async function findAll(req, res) {
         LEFT JOIN games g
         ON rentals."gameId" = g.id
         LEFT JOIN categories
-        ON g."categoryId"=categories.id;
-        `)
+        ON g."categoryId"=categories.id WHERE "customerId"=$1 OR "gameId"=$2;
+        `, [customerId, gameId])
         //RETURN QUERY AS JSON FORMAT row_to_json
         // const rentals = await connectionDb.query(`
         // select row_to_json(t)
@@ -47,32 +64,29 @@ export async function findAll(req, res) {
         //   from rentals
         // ) t;
         // `)
-
-        console.log(rentals.rows)
         const newRentalsObj = rentals.rows.map(r => {
             return {
-                id:r.id,
-                customerId:r.customerId,
-                gameId:r.gameId,
-                rentDate:r.rentDate,
-                daysRented:r.daysRented,
-                returnDate:r.returnDate,
-                originalPrice:r.originalPrice,
-                delayFee:r.delayFee,
-                customer:{
-                    id:r.customerId,
-                    name:r.customer_name
+                id: r.id,
+                customerId: r.customerId,
+                gameId: r.gameId,
+                rentDate: r.rentDate,
+                daysRented: r.daysRented,
+                returnDate: r.returnDate,
+                originalPrice: r.originalPrice,
+                delayFee: r.delayFee,
+                customer: {
+                    id: r.customerId,
+                    name: r.customer_name
                 },
-                game:{
-                    id:r.gameId,
-                    name:r.g_name,
-                    categoryId:r.categoryId,
-                    categoryName:r.category_name
+                game: {
+                    id: r.gameId,
+                    name: r.g_name,
+                    categoryId: r.categoryId,
+                    categoryName: r.category_name
                 }
             }
         })
-        console.log(newRentalsObj)
-        res.status(201).send(newRentalsObj);
+        res.status(200).send(newRentalsObj);
     } catch (error) {
         res.status(500).send(error.message)
     }
