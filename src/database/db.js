5f80bbbc4d5e1ecf1386a7bff8c53b43e167aaaf
Oscar Maia <oscarfgmaia@gmail.com>
2022-12-13 11:28:20 +0000

    refact: connection pool via string to remote database

--- src/database/db.js
+++ src/database/db.js
@@ -4,9 +4,6 @@ const {Pool} = pkg;
 dotenv.config();
 
 export const connectionDb = new Pool({
-    host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    connectionString:process.env.DATABASE_URL,
+    ssl:true
   });
